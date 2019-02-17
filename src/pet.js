23536cbf037678e4a62378cc5f9cd7f49be21c42
Emily Sayle <emsayle@gmail.com>
2019-02-17 16:20:36 +0000

    added tests for checkUp and isAlive methods and error for death msg, implemented working code

--- src/pet.js
+++ src/pet.js
@@ -1,31 +1,68 @@
+const MINIMUM_HUNGER = 0;
 const MAXIMUM_FITNESS = 10;
-const MINUMUM_HUNGER = 10;
+const DEATH_MSG = 'Your pet has died :\'(';
 
 function Pet(name) {
   this.name = name;
   this.age = 0;
-  this.hunger = MINUMUM_HUNGER;
+  this.hunger = MINIMUM_HUNGER;
   this.fitness = MAXIMUM_FITNESS;
 };
 
-Pet.prototype.growUp = function(){
+Pet.prototype = {
+  get isAlive() {
+    return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+  }
+};
+
+Pet.prototype.growUp = function() {
+  if (!this.isAlive) {
+    throw new Error(DEATH_MSG);
+  }
+  
   this.age += 1;
-  this.hunger += 5;
+  this.hunger += 4;
   this.fitness -= 3;
 };
 
 Pet.prototype.walk = function() {
+  if (!this.isAlive) {
+    throw new Error(DEATH_MSG);
+  }
+  
   if(this.fitness < 6) {
-  this.fitness += 4;
-} else {
-  this.fitness = MAXIMUM_FITNESS;
-};
+    this.fitness += 3;
+    this.hunger += 1;
+  } else (this.fitness = MAXIMUM_FITNESS);
 };
 
-Pet.prototype.feed = function () {
+Pet.prototype.feed = function() {
+  if (!this.isAlive) {
+    throw new Error(DEATH_MSG);
+  }
+  
   if(this.hunger > 3) {
-    this.hunger -= 3;
+    this.hunger -= 4;
+    this.fitness -= 1;
   } else (this.hunger = MINIMUM_HUNGER);
-}
+};
+
+Pet.prototype.checkUp = function() {
+  if (!this.isAlive) {
+    throw new Error(DEATH_MSG);
+  }
+  
+  if(this.fitness <= 3 && this.hunger >= 5) {
+    return 'I am hungry and I need a walk'
+  } else if(this.fitness <= 3) {
+    return 'I need a walk'
+  } else if(this.hunger >= 5) {
+    return 'I am hungry';
+  } else {
+    return 'I am just fine thanks!';
+  };
+};
+
+
 
 module.exports = Pet;
--- tests/pet.test.js
+++ tests/pet.test.js
@@ -17,9 +17,7 @@ describe('constructor', () => {
 describe('growUp function', () => {
   it('increases the pet\'s age by 1 when called', () => {
     pet.growUp();
-    pet.growUp();
-    pet.growUp();
-    expect(pet.age).toEqual(3);
+    expect(pet.age).toEqual(1);
   });
 });
 
@@ -43,4 +41,60 @@ describe('Pet health and hunger levels', () => {
     pet.feed();
     expect(pet.hunger).toEqual(2);
   })
+});
+
+describe('checkUp method', () => {
+  it('checks the pet\'s fitness status', () => {
+    pet.hunger = 4;
+    pet.fitness = 2;
+    expect(pet.checkUp()).toEqual('I need a walk');
+  });
+  it('checks the pet\'s hunger status', () => {
+    pet.hunger = 5;
+    expect(pet.checkUp()).toEqual('I am hungry');
+  });
+  it('checks both hunger and fitness together', () => {
+    pet.hunger = 6;
+    pet.fitness = 3;
+    expect(pet.checkUp()).toEqual('I am hungry and I need a walk');
+  });
+  it('checks both hunger and fitness together', () => {
+    pet.hunger = 4;
+    pet.fitness = 8;
+    expect(pet.checkUp()).toEqual('I am just fine thanks!');
+  });
+})
+
+describe('isAlive', () => {
+  it('returns a boolean whether the cat is still alive', () => {
+    pet.hunger = 10;
+    pet.fitness = 8;
+    pet.age = 10;
+    expect(pet.isAlive).toEqual(false);
+  });
+  it('returns a boolean whether the pet is still alive', () => {
+    pet.hunger = 4;
+    pet.fitness = 8;
+    pet.age = 10;
+    expect(pet.isAlive).toEqual(true);
+  });
+});
+
+describe('deathMsg', () => {
+  it('checkUp() thows an error message when the pet is no loner alive', () => {
+    pet.hunger = 10;
+    expect(pet.checkUp).toThrow('Your pet has died :\'(');
+  });
+  it('walk() thows an error message when the pet is no loner alive', () => {
+    pet.fitness = 0;
+    expect(pet.walk).toThrow('Your pet has died :\'(');
+  });
+  it('feed() thows an error message when the pet is no loner alive', () => {
+    pet.hunger = 10;
+    expect(pet.feed).toThrow('Your pet has died :\'(');
+});
+  it('growUp() thows an error message when the pet is no loner alive', () => {
+    pet.age = 30;
+    expect(pet.growUp).toThrow('Your pet has died :\'(');
+  });
 });
